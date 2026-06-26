5e1fdad987a929330799e759e459654033b7b460
Megvargian123 <mikekouyoumdjian@hotmail.com>
2026-06-26 09:39:44 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Homepage
+ */
+get_header();
+?>
+<?php
+	while ( have_posts() ) : the_post();
+        the_content();
+	endwhile;
+?>
+<script>
+    jQuery(document).ready(function($) {
+    })
+</script>
+<?
+get_footer();
